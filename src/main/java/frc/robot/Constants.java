b6c96840efa10edcec67395e835a0b2e7d908b65
SamRogers1 <samrog215@gmail.com>
2023-01-21 11:51:17 -0500

    worked on pid command

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,10 +3,10 @@ package frc.robot;
 public final class Constants {
 //we need measurements. Fighting with mechanical rn to use metric over feet measurements.
 public static final class BalanceConstants{
-    public static final double kTurnP = 1;
-    public static final double kTurnI = 0;
-    public static final double kTurnD = 0;
-
+    public static final double kP = 1;
+    public static final double kI = 0;
+    public static final double kD = 0;
+    public static final double kErrorThreshold = 1.0;
 
 }
 

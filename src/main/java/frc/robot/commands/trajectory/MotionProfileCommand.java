dc87dd44dc9b0d75c53c964bbcc82e2b2fb058e1
BananasAmIRite <jasony1230@gmail.com>
2023-02-04 13:51:43 -0500

    added support for new command system

--- src/main/java/frc/robot/commands/trajectory/MotionProfileCommand.java
+++ src/main/java/frc/robot/commands/trajectory/MotionProfileCommand.java
@@ -3,7 +3,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.trajectory;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.RamseteController;

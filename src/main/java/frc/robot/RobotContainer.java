7a00b7f8191a0aed2dba970e72932dcf3f8b00d6
BananasAmIRite <jasony1230@gmail.com>
2023-02-04 13:51:43 -0500

    added support for new command system

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,10 +2,15 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.util.XboxController;
+import org.bananasamirite.robotmotionprofile.ParametricSpline;
+import org.bananasamirite.robotmotionprofile.TankMotionProfile;
+import org.bananasamirite.robotmotionprofile.Waypoint;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class RobotContainer {
 
@@ -14,19 +19,22 @@ public class RobotContainer {
   private final XboxController manipulatorController = new XboxController(1);
 
   /*   Subsystems   */
-  private final Drivetrain drivetrain = new Drivetrain(driverController);
-
-  
+  private final Drivetrain drivetrain = new Drivetrain(driverController);  
 
   private final SendableChooser<Command> autoChooser = new SendableChooser<>();
 
   public RobotContainer() {
     configureButtonBindings();
 
+    List<Waypoint> waypoints = new ArrayList<>();
+
+    new TankMotionProfile(ParametricSpline.fromWaypoints(waypoints),
+            TankMotionProfile.ProfileMethod.TIME,
+            new TankMotionProfile.TankMotionProfileConstraints(1, 1));
+
     this.drivetrain.setDefaultCommand(new RunCommand(
             () -> this.drivetrain.arcadeDrive(driverController.getAxisValue(XboxController.Axis.LEFT_Y), driverController.getAxisValue(XboxController.Axis.RIGHT_X)),
             drivetrain));
-
     /* Add autos here */
     // autoChooser.addOption("name", auto);
   }

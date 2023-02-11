50bd08571b894ff6749f2c306c61c8a817f39d1a
collinsch2 <collinschokki@gmail.com>
2023-02-11 13:31:49 -0500

    assigned button for grabber

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,13 +2,23 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
+import frc.robot.Subsystems.Grabber.Grabber;
+import frc.robot.Subsystems.Grabber.commands.UseGrabber;
+import frc.robot.util.XboxController;
+import frc.robot.util.XboxController.Button;
 
 public class RobotContainer {
+  private final XboxController manipulatorController = new XboxController(1);
+  private final Grabber grabber = new Grabber();
+  private final UseGrabber uGrabber = new UseGrabber(grabber);
+  
   public RobotContainer() {
     configureButtonBindings();
   }
   
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    manipulatorController.whenPressed(Button.X, uGrabber);
+  }
 
   public Command getAutonomousCommand() {
     return new PrintCommand("No Auto Written Yet");

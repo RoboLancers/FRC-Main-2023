c82fe0d2e9c83ecefc0d91b7782fc1b245e50560
mattperls-code <mattperls.code@gmail.com>
2023-02-18 13:55:14 -0500

    formatting, TODO: test

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,19 +1,19 @@
 package frc.robot;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.subsystems.Grabber.Grabber;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.util.Controller;
 
 public class RobotContainer {
-  private final XboxController manipulatorController = new XboxController(0);
+  private final Controller driverController = new Controller(0);
+  
   private final Grabber grabber = new Grabber();
-  //private final UseGrabber uGrabber = new UseGrabber(grabber);
-  private final Pneumatics pneumatics = new Pneumatics();
+
+  private final Drivetrain drivetrain = new Drivetrain();
 
   public RobotContainer() {
     configureButtonBindings();
@@ -28,17 +28,16 @@ public class RobotContainer {
     // Controller.onPress(driverController.B, new InstantCommand(() -> {
     //   this.drivetrain.resetOdometry(new Pose2d(5, 5, new Rotation2d()));
     // }));
-    
-    manipulatorController.whenPressed(XboxController.Button.A, new InstantCommand(() -> {
-      grabber.toggleDeploy();
-    }));
 
-    /* Add autos here */
-    // autoChooser.addOption("name", auto);
+    Controller.onPress(driverController.A, new InstantCommand(grabber::toggleDeploy));
+  }
+
+  private void configureButtonBindings(){
+
   }
 
   public Command getAutonomousCommand() {
-    return autoChooser.getSelected();
+    return new InstantCommand();
   }
 
   public void doSendables(){

65ca00578ef1722f35da2063e787872211a7443e
Gitter499 <rafayel.amirkhanyan@gmail.com>
2023-02-20 14:13:02 -0500

    Added selection

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,25 +1,12 @@
 package frc.robot;
 
-import java.util.ArrayList;
-
-import org.bananasamirite.robotmotionprofile.ParametricSpline;
-import org.bananasamirite.robotmotionprofile.TankMotionProfile;
-import org.bananasamirite.robotmotionprofile.TankMotionProfile.ProfileMethod;
-import org.bananasamirite.robotmotionprofile.Waypoint;
-
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.math.trajectory.constraint.DifferentialDriveVoltageConstraint;
+
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.GridAlign;
-import frc.robot.commands.MotionProfileCommand;
 import frc.robot.commands.Rumble;
 import frc.robot.commands.GRR.TeleopGRR;
 import frc.robot.subsystems.drivetrain.Drivetrain;
@@ -30,17 +17,17 @@ import frc.robot.util.enums.Displacement;
 import frc.robot.util.limelight.LimelightAPI;
 
 public class RobotContainer {
-    /* Controllers */
-    private final Controller driverController = new Controller(0);
-    private final Controller manipulatorController = new Controller(1);
+  /* Controllers */
+  private final Controller driverController = new Controller(0);
+  private final Controller manipulatorController = new Controller(1);
 
-    /* Subsystems */
-    private final Drivetrain drivetrain = new Drivetrain(driverController);
-    private final PoseTracker poseTracker = new PoseTracker(drivetrain);
+  /* Subsystems */
+  private final Drivetrain drivetrain = new Drivetrain(driverController);
+  private final PoseTracker poseTracker = new PoseTracker(drivetrain);
 
   private final SendableChooser<Command> autoChooser = new SendableChooser<>();
 
-  private Command command; 
+  private Command command;
 
   public RobotContainer() {
     Controller.onPress(driverController.A, new InstantCommand(this.drivetrain::zeroHeading));
@@ -49,30 +36,35 @@ public class RobotContainer {
       drivetrain.resetOdometry(new Pose2d());
     }));
 
-  
-
     Controller.onPress(driverController.X, new ConditionalCommand(
-      // on true, instantiate and schedule align command
-      new TeleopGRR()
-      // on false rumble for 1 second
-      new Rumble(driverController, Constants.GridAlign.kRumbleTime),
-      // conditional upon a valid april tag
-      LimelightAPI::validTargets
-    ));
+        // on true, instantiate and schedule align command
+        new TeleopGRR(() -> {
+          if (driverController.dPadAngle() == 90) {
+            return Displacement.RIGHT;
+          } else if (driverController.dPadAngle() == 270) {
+            return Displacement.LEFT;
+          } else if (driverController.dPadAngle() == 0) {
+            return Displacement.CENTER;
+          } else {
+            return Displacement.CENTER;
+          }
 
-  
+        }),
+        // on false rumble for 1 second
+        new Rumble(driverController, Constants.GridAlign.kRumbleTime),
+        // conditional upon a valid april tag
+        LimelightAPI::validTargets));
 
     Controller.onPress(driverController.Y, new ConditionalCommand(
-      // on true, instantiate and schedule align command
-      new InstantiatorCommand(() -> new GridAlign(drivetrain, poseTracker, Displacement.CENTER)),
-      // on false rumble for 1 second
-      new Rumble(driverController, Constants.GridAlign.kRumbleTime),
-      // conditional upon a valid april tag
-      LimelightAPI::validTargets
-    ));
+        // on true, instantiate and schedule align command
+        new InstantiatorCommand(() -> new GridAlign(drivetrain, poseTracker, () -> Displacement.CENTER)),
+        // on false rumble for 1 second
+        new Rumble(driverController, Constants.GridAlign.kRumbleTime),
+        // conditional upon a valid april tag
+        LimelightAPI::validTargets));
   }
 
   public Command getAutonomousCommand() {
-    return command; 
+    return command;
   }
 }

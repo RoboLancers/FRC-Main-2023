a45b3aa493c1142c7d2e4aaa046692ebb0b51683
BananasAmIRite <jasony1230@gmail.com>
2023-03-11 18:04:14 -0500

    fixed mismatched controls

--- src/main/java/frc/robot/util/ManipulatorController.java
+++ src/main/java/frc/robot/util/ManipulatorController.java
@@ -16,11 +16,11 @@ public class ManipulatorController extends Controller {
     public ManipulatorController(int port) {
         super(port);
         
-        this.outtakeElementTriggerFast = new Trigger(() -> -getLeftStickY() > 0.2);
-        this.intakeElementTriggerFast = new Trigger(() -> -getLeftStickY() < -0.2);
+        this.outtakeElementTriggerFast = new Trigger(() -> -getRightStickY() > 0.2);
+        this.intakeElementTriggerFast = new Trigger(() -> -getRightStickY() < -0.2);
 
-        this.outtakeElementTriggerSlow = new Trigger(() -> -getRightStickY() > 0.2);
-        this.intakeElementTriggerSlow = new Trigger(() -> -getRightStickY() < -0.2);
+        this.outtakeElementTriggerSlow = new Trigger(() -> -getLeftStickY() > 0.2);
+        this.intakeElementTriggerSlow = new Trigger(() -> -getLeftStickY() < -0.2);
 
         this.dPadUp = new Trigger(() -> this.dPadAngle() == 0);
         this.dPadDown = new Trigger(() -> this.dPadAngle() == 180);

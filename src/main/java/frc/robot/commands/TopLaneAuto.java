d2750a5c0a1afb11250a69c658d508e8ca425776
collinsch2 <collinschokki@gmail.com>
2023-03-17 14:31:31 -0400

    made top lane cube support turning

--- src/main/java/frc/robot/commands/TopLaneAuto.java
+++ src/main/java/frc/robot/commands/TopLaneAuto.java
@@ -1,14 +1,41 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.drivetrain.commands.MoveBackward;
+import frc.robot.subsystems.drivetrain.commands.TurnBy;
 import frc.robot.subsystems.intake.Intake;
 
 public class TopLaneAuto extends SequentialCommandGroup {
     public TopLaneAuto(Drivetrain drivetrain, Arm arm, Intake intake, Constants.Arm.ScoringPosition position) {
-        addCommands(new Score(arm, intake, position), new MoveBackward(drivetrain, 3));
+        // addCommands(new Score(arm, intake, position), new MoveBackward(drivetrain, 3));
+        double HIGH_TURN_ANGLE_RED = -30; 
+        double allianceMultiplier = DriverStation.getAlliance() == Alliance.Red ? 1 : -1; 
+        switch(position) {
+            case HIGH_CUBE: 
+            case MID_CUBE:
+            case LOW_CUBE: 
+                addCommands(
+                    new Score(arm, intake, position), 
+                    new MoveBackward(drivetrain, 0.5), 
+                    new TurnBy(drivetrain, HIGH_TURN_ANGLE_RED * allianceMultiplier), 
+                    new MoveBackward(drivetrain, 0.5), 
+                    new TurnBy(drivetrain, -HIGH_TURN_ANGLE_RED * allianceMultiplier), 
+                    new MoveBackward(drivetrain, 4)
+                );
+                break;
+            case HIGH_CONE: 
+            case MID_CONE:
+            case LOW_CONE:
+                addCommands(
+                    new Score(arm, intake, position), 
+                    new MoveBackward(drivetrain, 5)
+                ); 
+                break; 
+        }
     }
 }
